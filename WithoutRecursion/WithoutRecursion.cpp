67399648ff0d9a55f94fdaff4c1e3595f843163f
Марат <kusegenov1029@mail.ru>
2021-06-25 00:55:06 +0300

    Добавьте файлы проекта.

---
+++ WithoutRecursion/WithoutRecursion.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+unsigned long int fib(unsigned long int n)
+{
+	if (n == 1 || n == 2) return 1;
+	if (n == 0) return 0;
+	return fib(n - 1) + fib(n - 2);
+}
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+	int a;
+	cout << "Введите a " << endl;
+	cin >> a;
+	cout << fib(a) << " " << " - Ваше число";
+	cout << endl;
+	system("pause");
+	return 0;
+}
